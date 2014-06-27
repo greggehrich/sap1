591f51d60b531bbb967252484dea0e34ff05b9f3
Greg Gehrich <ggehrich@gmail.com>
2014-06-26 21:47:57 -0700

    add precompile

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,22 @@
+class StaticPagesController < ApplicationController
+  def home
+    # require 'rubygems'
+    # require 'nokogiri'
+    # require 'open-uri'
+    # require 'uri'
+
+    @url = "http://www.zagat.com/r/marnee-thai-restaurant-san-francisco"
+    # puts "Story url is: #{url}"
+    @domain = URI(@url).host.match(/[^\.]+\.\w+$/).to_s
+    # puts "Domain is : #{domain}"
+    doc = Nokogiri::HTML(open(@url))
+    @title = doc.at_css("#main-content-title").text
+    # puts "Title is: #{title}"
+    @content = doc.at_css(".place-review-body p").text
+    # puts "Content is: #{content}"
+    @addr_phone = doc.at_css(".place-resume > p").text
+    # puts "Addr/Phone is: #{addr_phone}"
+    @story_website = doc.at_css(".website").text
+    # puts "Story website is: #{story_website}"
+  end
+end
